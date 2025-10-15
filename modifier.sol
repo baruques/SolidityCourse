79b020d32659c9363ca6c86da055f755b776e593
baruques <baruquechou@gmail.com>
2025-10-15 16:18:58 -0300

    Owner changing with modifiers

---
+++ modifier.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract testModifier {
+    address public owner;
+    uint256 public x = 10;
+    bool public locked;
+
+    constructor() {
+        owner = msg.sender; 
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner"); // Recebe como parâmetro a condição e, após a virgula, o retorno caso Falso
+        _; 
+    }
+
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Invalid address"); // Recebe como parâmetro a condição e, após a virgula, o retorno caso Falso
+        _; // O _ é um placeholder para a função que vai ser executada
+    }   
+
+    function changeOwner(address _newOwner) public onlyOwner validAddress(_newOwner) { // Já utiliza os modifiers, fazendo as verificações antes do código.
+        owner = _newOwner;
+    }
+}
+
